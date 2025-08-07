7c6ec5689e7493a0fcc896f9fbb3c33c16276319
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-07 07:29:21 -0700

    add detail hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen' >
-      
+    <div className="px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen">
+      <div className="text-center mb-6">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]">
+          Create Amazing Content <br />{" "}
+          <span className="text-purple-600">With Cnergy AI Tools</span>
+        </h1>
+        <p className="mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600">
+          Transform your content creation with our suite of premium AI tools.
+          write articles,generate ai images,and enhance your workflow
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
