8895961be18f8229039bb42944ee48551c19930b
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-09 03:30:30 -0700

    add basic template for pricing page

---
+++ src/components/Plan.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Plan = () => {
+  return (
+    <div className='max-w-2xl mx-auto z-20 my-30'>
+    <div className='text-center'></div>
+    <h2>Choose Your Plan</h2>
+    <p>Start For free and scale up as you grow, Find the perfect plan fro your content creation needs</p>
+      
+    </div>
+  )
+}
+
+export default Plan
