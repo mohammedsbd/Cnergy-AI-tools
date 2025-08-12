adab9bcbc7410b7f404e2e0733a02569f5ada556
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-12 05:41:58 -0700

    add menu icon

--- src/components/pages/Layout.jsx
+++ src/components/pages/Layout.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../../assets/assets'
+import { MenuIcon, X } from 'lucide-react'
 
 
 const Layout = () => {
   const navigate=useNavigate()
+  const [sidebar,setSidebar]=useState(false)
   return (
     <div className='flex flex-col min-h-screen h-screen justify-start items-start'>
-    <nav><img src={assets.logo} alt="" onClick={()=>navigate('/')}/></nav>
+    <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'><img src={assets.logo} alt="" onClick={()=>navigate('/')}/>
+    {
+sidebar? <X className='w-6 h-6 text-gray-600 sm:hidden'/>:<MenuIcon className='w-6 h-6 text-gray-600 sm:hidden'/>
+    }
+    </nav>
       <Outlet />
     </div>
   )
