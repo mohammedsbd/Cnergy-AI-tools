8efb04a47c143f77b29640e483715160bece538e
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-13 01:10:37 -0700

    add sidebar config

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useClerk, useUser } from "@clerk/clerk-react";
+import React from "react";
 
-const Sidebar = ({Sidebar,setSidebar}) => {
+const Sidebar = ({ sidebar, setSidebar }) => {
+  const user=useUser();
+  const {signOut,openUserProfile} = useClerk();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div
+      className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${
+        sidebar ? "translate-x-0" : "max-sm:-translate-x-full"
+      } transition-all duration-300 ease-in-out`}
+    >
+      <div className="my-7 w-full">
+      <img src={user.imageUrl} alt="" className="w-13 rounded-full mx-auto" />
 
-export default Sidebar
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
