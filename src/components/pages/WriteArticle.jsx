81ae148ebaeaaa86b21ece6d74c772c12ba0f653
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-15 11:52:21 -0700

    add expand content

--- src/components/pages/WriteArticle.jsx
+++ src/components/pages/WriteArticle.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 
 const WriteArticle = () => {
   return (
-    <div className='h-full overflow-y-scroll p-6 flex item-start flex-wrap gap-4 text-slate-700'>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
   {/* {left -col} */}
   <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
-<div>
-  <Sparkles className='w-6 text-[#4A7AFF]'/>    
+<div className='flex items-center gap-3'>
+  <Sparkles className='w-6 text-[#4A7AFF]'/> 
+  <h1 className='text-xl font-semibold '>Article Configuration</h1>   
 </div>
+<p className='mt-6 text-sm font-medium'>Article Topic</p>
+<input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 ' placeholder='The future of artificial intelligence is...'/>
   </form>
   {/* {rigth-column} */}
     </div>
