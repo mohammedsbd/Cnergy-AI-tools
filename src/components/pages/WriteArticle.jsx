33ab725d30fec6ce13597ffaf8dbdaaad99ec2f8
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-15 14:32:14 -0700

    add expand content

--- src/components/pages/WriteArticle.jsx
+++ src/components/pages/WriteArticle.jsx
@@ -1,7 +1,13 @@
 import { Sparkle, Sparkles } from 'lucide-react'
 import React from 'react'
+import { text } from './../../../node_modules/mdast-util-to-hast/lib/handlers/text';
 
 const WriteArticle = () => {
+  const articleLength=[
+    {length:800, text:'Short (500-800 words)'},
+    {length:1200, text:'Medium (800-1200 words)'},
+    {length:1600, text:'Long (1200+ words)'},
+  ]
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
   {/* {left -col} */}
@@ -11,7 +17,13 @@ const WriteArticle = () => {
   <h1 className='text-xl font-semibold '>Article Configuration</h1>   
 </div>
 <p className='mt-6 text-sm font-medium'>Article Topic</p>
-<input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 ' placeholder='The future of artificial intelligence is...'/>
+<input type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 ' placeholder='The future of artificial intelligence is...' required/>
+<p className='mt-4 text-sm font-medium'>Article Length</p>
+<div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+  {articleLength.map((item,index) => (
+    <span  className='text-xs px-4 py-1 border rounded-full cursor-pointer' key={index}>{item.text}</span>
+  ))}
+</div>
   </form>
   {/* {rigth-column} */}
     </div>
