891f03fe1705ecff8f190a1bbbe9005a471d7d79
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-16 12:59:31 -0700

    Update GenerateImages.jsx

--- src/components/pages/GenerateImages.jsx
+++ src/components/pages/GenerateImages.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
+import { Hash, Image, Sparkles } from 'lucide-react';
+
 
 const GenerateImages = () => {
-    const blogCategories = [
-      'General ', 'Technology' ,'Business', "Health", "LifeStyle", "Education" ,"Travel", "Food"
+    const imageStyle = [
+      'Realistic ', 'Ghibli Style' ,'Anime Style', "Cartoon Style", "Fantasy Style", "Realistic Style " ,"3D style", "Portrait Style"
       ];
     
-      const [selectedCategory, setSelectedCategory] = useState("General");
+      const [selectedStyle, setSelectedStyle] = useState("Realistic");
       const [input, setInput] = useState("");
+      const [publish,setPublish]=useState(false)
     
       const onSubmitHandler=async (e)=>{
         e.preventDefault()
@@ -20,27 +24,27 @@ const GenerateImages = () => {
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#8E37EB]" />
-          <h1 className="text-xl font-semibold">AI Title Generator</h1>
+          <Sparkles className="w-6 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold">AI Image Generator</h1>
         </div>
 
-        <p className="mt-6 text-sm font-medium">Keyword</p>
-        <input
+        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <textarea
+          rows={4}
           onChange={(e) => setInput(e.target.value)}
           value={input}
-          type="text"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
-          placeholder="The future of artificial intelligence is..."
+          placeholder="Describe Your Image..."
           required
         />
 
-        <p className="mt-4 text-sm font-medium">Category</p>
+        <p className="mt-4 text-sm font-medium">Style</p>
         <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
-          {blogCategories.map((item) => (
+          {imageStyle.map((item) => (
             <span
-              onClick={() => setSelectedCategory(item)}
+              onClick={() => setSelectedStyle(item)}
               className={`text-xs px-4 py-1 border rounded-full cursor-pointer transition ${
-                selectedCategory === item
+                selectedStyle === item
                   ? "bg-purple-50 text-purple-700 border-blue-400"
                   : "text-gray-500 border-gray-300 hover:bg-gray-100"
               }`}
@@ -51,22 +55,22 @@ const GenerateImages = () => {
           ))}
         </div>
         <br />
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E373B] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
-          <Hash className="w-5" />
-          Generate Title
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#04FF50] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
+          <Image className="w-5" />
+          Generate Image
         </button>
       </form>
       {/* right-col */}
 
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
         <div className="flex items-center gap-3 ">
-          <Hash className="w-5 h-5 text-[#8E37EB]" />
-          <h1 className="text-xl font-semibold ">Generated Titles</h1>
+          <Image className="w-5 h-5 text-[#00AD25]" />
+          <h1 className="text-xl font-semibold ">Generated Image</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Hash className="w-9 h-9 " />
-            <p>Enter Topic and click "Generate Title" to get started</p>
+            <Image className="w-9 h-9 " />
+            <p>Enter Topic and click "Generate Image" to get started</p>
           </div>
         </div>
       </div>
