d51bae1f065edabfa57ce8ff66f48dd6904d2888
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-16 13:23:42 -0700

    Update RemoveBackground.jsx

--- src/components/pages/RemoveBackground.jsx
+++ src/components/pages/RemoveBackground.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const RemoveBackground = () => {
+
+  const [input, setInput] = useState("");
+    
+      const onSubmitHandler=async (e)=>{
+        e.preventDefault()
+    
+      }
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/* left-col */}
