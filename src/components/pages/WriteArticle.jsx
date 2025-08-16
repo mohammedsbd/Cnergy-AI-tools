d2966de4eaddb573b2c0fed33605b47b648e9f65
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-16 11:22:33 -0700

    Update WriteArticle.jsx

--- src/components/pages/WriteArticle.jsx
+++ src/components/pages/WriteArticle.jsx
@@ -11,10 +11,15 @@ const WriteArticle = () => {
   const [selectedLength, setSelectedLength] = useState(articleLength[0]);
   const [input, setInput] = useState("");
 
+  const onSubmitHandler=async (e)=>{
+    e.preventDefault()
+
+  }
+
   return (
     <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
       {/* left-col */}
-      <form className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+      <form onSubmit={onSubmitHandler} className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
         <div className="flex items-center gap-3">
           <Sparkles className="w-6 text-[#4A7AFF]" />
           <h1 className="text-xl font-semibold">Article Configuration</h1>
