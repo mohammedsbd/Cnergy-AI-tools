b0563baf186c57b1556d49406bc200887f398957
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-16 12:40:44 -0700

    Update GenerateImages.jsx

--- src/components/pages/GenerateImages.jsx
+++ src/components/pages/GenerateImages.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const GenerateImages = () => {
+    const blogCategories = [
+      'General ', 'Technology' ,'Business', "Health", "LifeStyle", "Education" ,"Travel", "Food"
+      ];
+    
+      const [selectedCategory, setSelectedCategory] = useState("General");
+      const [input, setInput] = useState("");
+    
+      const onSubmitHandler=async (e)=>{
+        e.preventDefault()
+    
+      }
   return (
     <div>
       <h1>Gnerateimages</h1>
