f9cc349b21ede6a9a5822b91a5c16c5042501c27
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-16 13:23:14 -0700

    update remove backround ui

--- src/components/pages/GenerateImages.jsx
+++ src/components/pages/GenerateImages.jsx
@@ -31,11 +31,12 @@ const GenerateImages = () => {
         <p className="mt-6 text-sm font-medium">Describe Your Image</p>
         <textarea
           rows={4}
-          onChange={(e) => setInput(e.target.value)}
+          onChange={(e) => setInput(e.target.checked)}
           value={input}
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
           placeholder="Describe Your Image..."
           required
+          checked={publish}
         />
 
         <p className="mt-4 text-sm font-medium">Style</p>
@@ -55,19 +56,19 @@ const GenerateImages = () => {
           ))}
         </div>
 
-        <div className='my-6 flex items-center gap-2'>
-        <label className='relative cursor-pointer'>
-          <input type="checkbox" onChange={(e)=>setPublish(e.target.value)} checked={publish} className='sr-only peer' />
-          <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'>
-
-          </div>
-          <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4'></span>
-        </label>
-        <p className='text-sm'>Make this image public</p>
-
+        <div className="my-6 flex items-center gap-2">
+          <label className="relative cursor-pointer">
+            <input
+              type="checkbox"
+              onChange={(e) => setPublish(e.target.value)}
+              // checked={publish}
+              className="sr-only peer"
+            />
+            <div className="w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition"></div>
+            <span className="absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4"></span>
+          </label>
+          <p className="text-sm">Make this image public</p>
         </div>
-       
-
 
         <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#00AD25]  to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
           <Image className="w-5" />
