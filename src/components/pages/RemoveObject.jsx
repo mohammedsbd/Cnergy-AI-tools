a802ae987c116816f00fecaac0456d86ce0daf22
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 12:26:29 -0700

    Update RemoveObject.jsx

--- src/components/pages/RemoveObject.jsx
+++ src/components/pages/RemoveObject.jsx
@@ -1,4 +1,4 @@
-import { Sparkles } from 'lucide-react';
+import { Scissors, Sparkles } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react';
 
@@ -33,32 +33,32 @@ const RemoveObject = () => {
           required
         />
 
-        <p className="mt-6 text-sm font-medium">Describe Your Image</p>
+        <p className="mt-6 text-sm font-medium">Describe Object Name To Remove</p>
         <textarea
           rows={4}
-          onChange={(e) => setInput(e.target.checked)}
-          value={input}
+          onChange={(e) => setObject(e.target.value)}
+          value={object}
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
-          placeholder="Describe Your Image..."
+          placeholder="e.g watch or spoon,Only single object name..."
           required
           checked={publish}
         />
-        <button className=" cursor-pointer w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
-          <Eraser className="w-5 " />
-          Remove Background
+        <button className=" cursor-pointer w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
+          <Scissors className="w-5 " />
+          Remove Object
         </button>
       </form>
       {/* right-col */}
 
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
         <div className="flex items-center gap-3 ">
-          <Eraser className="w-5 h-5 text-[#FF4938]" />
+          <Scissors className="w-5 h-5 text-[#4A7AFF]" />
           <h1 className="text-xl font-semibold ">Processed Image</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Eraser className="w-9 h-9 " />
-            <p>Upload an image and click "Remove Background" to get started</p>
+            <Scissors className="w-9 h-9 " />
+            <p>Upload an image and click "Remove Object" to get started</p>
           </div>
         </div>
       </div>
