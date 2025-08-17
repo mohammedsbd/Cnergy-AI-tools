6484818b220bb9742bea15bc5d4851500d2e3705
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 12:16:30 -0700

    Update RemoveObject.jsx

--- src/components/pages/RemoveObject.jsx
+++ src/components/pages/RemoveObject.jsx
@@ -21,7 +21,7 @@ const RemoveObject = () => {
           <h1 className="text-xl font-semibold">Background Removal</h1>
         </div>
 
-         <p className="mt-6 text-sm font-medium">Upload Image</p>
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
         <input
           onChange={(e) => setInput(e.target.files[0])}
           type="file"
@@ -30,8 +30,7 @@ const RemoveObject = () => {
           required
         />
 
-
-           <p className="text-xs text-gray-500 font-light mt-1 ">
+        <p className="text-xs text-gray-500 font-light mt-1 ">
           Supports JPG,PNG and other image format
         </p>
         <button className=" cursor-pointer w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
@@ -39,17 +38,21 @@ const RemoveObject = () => {
           Remove Background
         </button>
       </form>
-
-       {/* right-col */}
+      {/* right-col */}
 
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
         <div className="flex items-center gap-3 ">
           <Eraser className="w-5 h-5 text-[#FF4938]" />
           <h1 className="text-xl font-semibold ">Processed Image</h1>
         </div>
-
-
-
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Eraser className="w-9 h-9 " />
+            <p>Upload an image and click "Remove Background" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
