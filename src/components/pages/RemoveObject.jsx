b64fafcb2827f5f9310055c38c7f093987d6883d
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 12:12:44 -0700

    Update RemoveObject.jsx

--- src/components/pages/RemoveObject.jsx
+++ src/components/pages/RemoveObject.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
 const RemoveObject = () => {
+   const [input, setInput] = useState("");
+      
+        const onSubmitHandler=async (e)=>{
+          e.preventDefault()
+      
+        }
 
- const [input, setInput] = useState("");
-    
-      const onSubmitHandler=async (e)=>{
-        e.preventDefault()
-    
-      }
-  https: return (
-    <div>
-      <h1>Remove Objects</h1>
-    </div>
+
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left-col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Background Removal</h1>
+        </div>
   );
 }
 
