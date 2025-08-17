bd3d84e132f623980d89b027ed61545296adf177
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 12:04:35 -0700

    Update RemoveBackground.jsx

--- src/components/pages/RemoveBackground.jsx
+++ src/components/pages/RemoveBackground.jsx
@@ -1,6 +1,6 @@
-import { Sparkles } from 'lucide-react';
+import { Eraser, Sparkles } from 'lucide-react';
 import React from 'react'
-
+import { useState } from 'react';
 const RemoveBackground = () => {
 
   const [input, setInput] = useState("");
@@ -34,22 +34,22 @@ const RemoveBackground = () => {
 
 
        <p className='text-xs text-gray-500 font-light mt-1 '>Supports JPG,PNG and other image format</p>
-        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E373B] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
-          <Hash className="w-5" />
-          Generate Title
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F6AB41] to-[#FF4938] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
+          <Eraser className="w-5" />
+       Remove Background
         </button>
       </form>
       {/* right-col */}
 
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 ">
         <div className="flex items-center gap-3 ">
-          <Hash className="w-5 h-5 text-[#8E37EB]" />
-          <h1 className="text-xl font-semibold ">Generated Titles</h1>
+          <Eraser className="w-5 h-5 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold ">Processed Image</h1>
         </div>
         <div className="flex-1 flex justify-center items-center">
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <Hash className="w-9 h-9 " />
-            <p>Enter Topic and click "Generate Title" to get started</p>
+            <Eraser className="w-9 h-9 " />
+            <p>Upload an image and click "Remove Background" to get started</p>
           </div>
         </div>
       </div>
