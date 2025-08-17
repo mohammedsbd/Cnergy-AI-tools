1a95a58e6ed892eb37b331c232fb388313c3b8bf
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 11:54:38 -0700

    Update RemoveBackground.jsx

--- src/components/pages/RemoveBackground.jsx
+++ src/components/pages/RemoveBackground.jsx
@@ -1,3 +1,4 @@
+import { Sparkles } from 'lucide-react';
 import React from 'react'
 
 const RemoveBackground = () => {
@@ -16,37 +17,23 @@ const RemoveBackground = () => {
         className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
       >
         <div className="flex items-center gap-3">
-          <Sparkles className="w-6 text-[#8E37EB]" />
-          <h1 className="text-xl font-semibold">AI Title Generator</h1>
+          <Sparkles className="w-6 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Background Removal</h1>
         </div>
 
-        <p className="mt-6 text-sm font-medium">Keyword</p>
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
         <input
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-          type="text"
-          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
-          placeholder="The future of artificial intelligence is..."
+          onChange={(e) => setInput(e.target.files[0])}
+      
+          type="file"
+          accept='image/*'
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+         
           required
         />
 
-        <p className="mt-4 text-sm font-medium">Category</p>
-        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
-          {blogCategories.map((item) => (
-            <span
-              onClick={() => setSelectedCategory(item)}
-              className={`text-xs px-4 py-1 border rounded-full cursor-pointer transition ${
-                selectedCategory === item
-                  ? "bg-purple-50 text-purple-700 border-blue-400"
-                  : "text-gray-500 border-gray-300 hover:bg-gray-100"
-              }`}
-              key={item}
-            >
-              {item}
-            </span>
-          ))}
-        </div>
-        <br />
+
+       <p className='text-xs text-gray-500 font-light mt-1 '>Supports JPG,PNG and other image format</p>
         <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#C341F6] to-[#8E373B] text-white px-4 py-2 mt-6 text-sm rounded-lg ">
           <Hash className="w-5" />
           Generate Title
