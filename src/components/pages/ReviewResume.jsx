450d91a7e270acf4ba657d0147a3b70b2f22d4dd
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-17 12:30:08 -0700

    Update ReviewResume.jsx

--- src/components/pages/ReviewResume.jsx
+++ src/components/pages/ReviewResume.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 const ReviewResume = () => {
   return (
-    <div>
-      <h1>Reviewresume</h1>
-    </div>
+   <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/* left-col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#FF4938]" />
+          <h1 className="text-xl font-semibold">Background Removal</h1>
+        </div>
+
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          type="file"
+          accept="image/*"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          required
+        />
   )
 }
 
