97556e08e821a9d3cd1395c2260522a7e2d7034e
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-20 11:00:11 -0700

    Update About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,18 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function AvatarGenerator() {
-  const [seed, setSeed] = useState(Math.random());
-  const generate = () => setSeed(Math.random());
+function MemeGenerator() {
+  const [templates, setTemplates] = useState([]);
+  const [meme, setMeme] = useState({ url: "", top: "", bottom: "" });
 
-  const url = `https://avatars.dicebear.com/api/human/${seed}.svg`;
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((json) => setTemplates(json.data.memes));
+  }, []);
+
+  const newMeme = () => {
+    const choice = templates[Math.floor(Math.random() * templates.length)];
+    setMeme((m) => ({ ...m, url: choice.url }));
+  };
 
   return (
     <div>
-      <h2>Avatar Generator</h2>
-      <img src={url} alt="Avatar" width="200" height="200" />
-      <button onClick={generate}>New Avatar</button>
+      <h2>Meme Generator</h2>
+      <button onClick={newMeme}>Random Meme</button>
+      {meme.url && (
+        <div
+          style={{
+            position: "relative",
+            display: "inline-block",
+            textAlign: "center",
+            marginTop: "12px",
+          }}
+        >
+          <img src={meme.url} alt="Meme" width="400" />
+          <div
+            style={{
+              position: "absolute",
+              top: "8px",
+              width: "100%",
+              color: "white",
+              fontSize: "24px",
+              textShadow: "2px 2px black",
+            }}
+          >
+            {meme.top}
+          </div>
+          <div
+            style={{
+              position: "absolute",
+              bottom: "8px",
+              width: "100%",
+              color: "white",
+              fontSize: "24px",
+              textShadow: "2px 2px black",
+            }}
+          >
+            {meme.bottom}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default AvatarGenerator;
+export default MemeGenerator;
