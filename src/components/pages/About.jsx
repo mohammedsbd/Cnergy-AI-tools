6c4eb00c3841ffbf619fcd535a728551985a7d3f
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-22 09:40:38 -0700

    Update About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,30 +1,17 @@
-function Notifications({ notifications }) {
-  const [open, setOpen] = React.useState(false);
-
+function BlogPreviewList({ posts }) {
   return (
-    <div style={{ position: "relative", display: "inline-block" }}>
-      <button onClick={() => setOpen(!open)}>
-        🔔 ({notifications.length})
-      </button>
-      {open && (
-        <div
-          style={{
-            position: "absolute",
-            top: "2rem",
-            right: 0,
-            background: "white",
-            border: "1px solid #ccc",
-            padding: "1rem",
-            width: "200px",
-          }}
-        >
-          <ul>
-            {notifications.map((n, i) => (
-              <li key={i}>{n}</li>
-            ))}
-          </ul>
+    <div>
+      <h2>Latest Posts</h2>
+      {posts.map((post) => (
+        <div key={post.id} style={{ marginBottom: "2rem" }}>
+          <h3>{post.title}</h3>
+          <p>{post.excerpt}</p>
+          <button onClick={() => alert(`Reading: ${post.title}`)}>
+            Read More
+          </button>
         </div>
-      )}
+      ))}
     </div>
   );
 }
+export default BlogPreviewList;
